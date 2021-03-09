8565ce516ddd45e2ce99e97c7cb0499ab3bb4c79
janejane <janetjanedchic@yahoo.com>
2021-03-09 15:09:12 -0500

    Setup: React Router for airbnb build

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="app">
+      <Router>
       <Header />
+      <Switch>
+        <Route pathe="/search">
+        <SearchPage />
+        </Route>
+      <Route path="/">
       <Home />
+      </Route>
+    
+      </Switch>
+      
       <Footer />
+      </Router>
+      
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css'
 
 function Footer() {
     return (
         <div className='footer'>
             <p>© 2021 Airbnb clone! No rights reserved - this is a demo!</p>
-            <p>Privacy · Terms · Sitemap · Company Details</p>
+            <p>Privacy · Terms · Sitemap · Contact: jossychamp2000@yahoo.com</p>
             
         </div>
     )
